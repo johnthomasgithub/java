d26bd90722d16c103664405c923a1aeb46852035
john <johnthomas222000@gmail.com>
2021-05-05 23:46:11 +0530

    commit

---
+++ Stringpool.java
@@ -0,0 +1,14 @@
+package logical;
+
+public class Stringpool {
+
+	public static void main(String[] args) {
+		String s = "John";
+		String str = "John";
+		if(s == str)
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+}
