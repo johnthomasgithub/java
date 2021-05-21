7b9e25aeffad86a8f2425f5edf9a3f0735c4c7f0
john <johnthomas222000@gmail.com>
2021-05-21 14:48:30 +0530

    commit

---
+++ NextGreaterArray.java
@@ -0,0 +1,29 @@
+package logical;
+import java.util.*;
+public class NextGreaterArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(arr[j] > arr[i])
+				{
+					System.out.print( arr[j] + " ");
+					break;
+				}
+				if(j==n-1)
+					System.out.print(-1+" ");
+			}
+		}
+
+	}
+
+}
