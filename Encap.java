afc3403df9e8ad66104ed2aab0c56b23746091b6
john <johnthomas222000@gmail.com>
2021-05-22 23:00:36 +0530

    commit

---
+++ Encap.java
@@ -0,0 +1,32 @@
+package logical;
+
+class Student{
+	String name;
+	int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
+
+
+public class Encap {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setAge(12);
+		s.setName("Tom");
+		System.out.println(s.getName());
+
+	}
+
+}
