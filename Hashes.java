60cf4ea170916a9eac349e69a912dd6218e2540b
john <johnthomas222000@gmail.com>
2021-05-25 18:39:31 +0530

    Commit

--- Hashes.java
+++ Hashes.java
@@ -1,18 +1,29 @@
 package logical;
 
-import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Hashes {
+	public static void main(String[] args)
+	{
 
-	public static void main(String[] args) {
-		HashSet<String> hs = new HashSet<String>();
-		ArrayList<String> ls = new ArrayList<String>();
-		ls.add("sv");
-		ls.add("aaa");
-		hs.add("john");
-		hs.add("thomas");
-		hs.add("tim");
-		hs.addAll(ls);
-		System.out.print(hs);
+		String str = "Alice is girl and Bob is boy";
+		Map<String, Integer> m = new HashMap<>();
+		String[] s = str.split(" ");
+		
+		for (String word : s) {			
+			if (m.containsKey(word))
+				m.put(word, m.get(word) + 1);
+			else
+				m.put(word, 1);
+		}
+		Iterator<Map.Entry<String, Integer>> it = m.entrySet().iterator();
+		while(it.hasNext())
+		{
+			Map.Entry<String, Integer> entry = it.next();
+			System.out.println(entry.getKey()+" =>"+ entry.getValue());
+		}
 	}
-
 }
---
+++ HsMap.java
@@ -0,0 +1,17 @@
+package logical;
+import java.util.*;
+public class HsMap {
+
+	public static void main(String[] args) {
+		 	HashMap<String,Integer> hs = new HashMap();
+		 	hs.put("a", 1);
+		 	hs.put("b", 2);
+		 	hs.put("c", 3);
+		 	hs.put("d", 4);
+		 	hs.put("e", 5);
+		 	hs.put("e", 6);
+		 	hs.put("f", 7);
+		 	System.out.println(hs);
+	}
+
+}
