c48004f104ed2c41615ceeffcb5f932fe76625c8
john <johnthomas222000@gmail.com>
2021-06-20 18:08:38 +0530

    aravind

---
+++ Aravind.java
@@ -0,0 +1,11 @@
+package logical;
+
+public class Aravind {
+
+	public static void main(String[] args) {
+		final int age = 18;
+		boolean Citizen = true;
+		System.out.println(age == 18 && Citizen);
+	}
+
+}
